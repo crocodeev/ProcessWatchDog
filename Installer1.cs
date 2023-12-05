2c7eb3bea53d965320966c5fd86aceff87cd052a
crocodeev <svin862003@gmail.com>
2023-12-05 19:47:30 +0300

    Add Installer

---
+++ Installer1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheshkaWatchDog
+{
+    [RunInstaller(true)]
+    public partial class Installer1 : System.Configuration.Install.Installer
+    {
+        public Installer1()
+        {
+            InitializeComponent();
+        }
+
+        public override void Install(IDictionary stateSaver)
+        {
+            base.Install(stateSaver);
+
+            string schedule = Context.Parameters["schedule"];
+
+            InstallerHelper.WriteToRegisry(schedule);
+
+        }
+    }
+}
---
+++ InstallerHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace CheshkaWatchDog
+{
+    public class InstallerHelper
+    {
+        public static void WriteToRegisry(string schedule)
+        {
+
+            string watchDogPath = "SOFTWARE\\Inplay\\CheshkaWatchDog\\";
+
+            try
+            {
+                RegistryKey root = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+
+                if (root.OpenSubKey(watchDogPath) == null)
+                {
+                    RegistryKey result = root.CreateSubKey(watchDogPath);
+           
+                }
+               
+
+                RegistryKey key = root.OpenSubKey(watchDogPath, true);
+
+                if (key == null) { throw new Exception("Can't create new key in registry"); }
+
+                key.SetValue("schedule", schedule);
+
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.StackTrace + ' ' + ex.Message);
+            }
+            
+        }
+
+        public void DeleteServive() { }
+
+        public void DeleteFolder() { }
+    }
+}
